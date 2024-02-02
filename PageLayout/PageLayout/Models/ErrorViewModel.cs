c9995037ef112db786becc4217838de0b642d64a
Thato <t.chitja369@gmail.com>
2024-02-02 15:54:14 +0200

    Initial Commit

---
+++ PageLayout/PageLayout/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PageLayout.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
